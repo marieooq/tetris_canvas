6d528e53bcc54d54add68d4f2eb8f5c8ecb917aa
Marie Otaki <otakimarie@gmail.com>
2019-09-15 16:22:38 -0700

    add collide function

--- js/tetris.js
+++ js/tetris.js
@@ -5,6 +5,26 @@ context.scale(20, 20);
 
 const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
 
+function collide(arena, player) {
+  const [m, o] = [player.matrix, player.pos];
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function createMatrix(w, h) {
+  const matrix = [];
+  while (h--) {
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+}
+
 function draw() {
   context.fillStyle = "#000";
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -23,8 +43,23 @@ function drawMatrix(matrix, offset) {
   });
 }
 
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+}
+
 function playerDrop() {
   player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
   dropCounter = 0;
 }
 
@@ -45,6 +80,8 @@ function update(time = 0) {
   draw();
   requestAnimationFrame(update);
 }
+
+const arena = createMatrix(12, 20);
 
 const player = {
   matrix: matrix,
